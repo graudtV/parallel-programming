e1dcf2da12a6ff188a79c6857c4f23e6dd0eba1b
graudtV <55806873+graudtV@users.noreply.github.com>
2022-03-05 17:57:30 +0300

    small fixes

--- envconfig.sh
+++ envconfig.sh
@@ -1,3 +1,5 @@
 # this should be run with 'source' command
 
+alias run='/home/b0190810/mpi-projects/tools/run'
 alias progrun='/home/b0190810/mpi-projects/tools/run prog'
+alias mprogrun='make && /home/b0190810/mpi-projects/tools/run prog'
